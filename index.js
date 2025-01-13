078479d976323e19f7c49a82917c1c3c72ee417e
springboot44 <springlearning44@gmail.com>
2025-01-13 05:12:30 +0000

    express

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
-const port = 3000
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('express server ')
 })
 
+const port=process.env.PORT || 6000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
